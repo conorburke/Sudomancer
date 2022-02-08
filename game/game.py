2fddd6ae0bcd211d1f280c0cdda587c42e1384d4
conorburke <cjburke89@gmail.com>
2022-02-08 14:23:53 +0900

    solve sudokus recursively, solve hardest known sudoku

--- game/game.py
+++ game/game.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 from math import sqrt
 
@@ -9,21 +10,25 @@ class Game():
         self.options = np.arange(1, size + 1)
         # self.num_blocks = int(sqrt(size))
         self.blocks = None
-        self.block_mapping = {}
+        # {block_row: {block_row_index: (board_row, board_column)}}
+        self.block_to_coordinate_mapping = {}
+        # {(board_row, board_column): block_row}
+        self.coordinate_to_block_mapping = {}
 
 
-    def create_blocks(self) -> np.ndarray:
+    def create_blocks(self, board: np.ndarray) -> np.ndarray:
         blocks = np.zeros((self.size, self.size))
         root = int(sqrt(self.size))
         mover_right = 0
         mover_down = 0
         for i in range(self.size):
-            self.block_mapping[i] = {}
+            self.block_to_coordinate_mapping[i] = {}
             counter = 0
             for x in range(mover_down, root + mover_down):
                 for y in range(mover_right, root + mover_right):
-                    blocks[i][counter] = self.board[x][y]
-                    self.block_mapping[i][counter] = (x, y)
+                    blocks[i][counter] = board[x][y]
+                    self.block_to_coordinate_mapping[i][counter] = (x, y)
+                    self.coordinate_to_block_mapping[(x,y)] = i
                     counter += 1
             if (i + 1) % root == 0:
                 mover_right = 0
@@ -32,19 +37,24 @@ class Game():
                 mover_right += root
         
         self.blocks = blocks
-        # print(self.block_mapping)
         return blocks        
 
     
+    def user_put_number(self, row: int, column: int, number: int, board: np.ndarray) -> np.ndarray:
+        """ Don't require use to think about zero-indexing """
+        return self.put_number(row - 1, column - 1, number, board)
+
+    
     def put_number(self, row: int, column: int, number: int, board: np.ndarray) -> np.ndarray:
-        # get_possibilites is 1-indexed not 0 but board is 0 indexed so adjust
-        board[row - 1][column - 1] = number
+        board[row][column] = number
         return board
 
 
+    def get_block_from_coordinates(self, x: int, y: int) -> int:
+        return self.coordinate_to_block_mapping[(x, y)]
+
+
     def get_row_column_intersection(self, row, column, board) -> np.ndarray:
-        row -= 1
-        column -= 1
         row_possibilities = np.setdiff1d(self.options, board[row])
         column_possibilites = np.setdiff1d(self.options, board.T[column])
         intersection = np.intersect1d(row_possibilities, column_possibilites)
@@ -54,15 +64,20 @@ class Game():
     def get_block_intersection(self, block_num: int) -> tuple:
         if self.blocks is None:
             return None
-        block_possibilities = np.setdiff1d(self.options, self.blocks[block_num])
+        # block_possibilities = np.setdiff1d(self.options, self.blocks[block_num])
+        block_possibilities = self.get_block_possibilites(block_num, self.blocks)
         if block_possibilities.size == 1:
             value = block_possibilities[0]
             i = np.where(self.blocks[block_num]==0)[0][0]
-            board_index = self.block_mapping[block_num][i]
+            board_index = self.block_to_coordinate_mapping[block_num][i]
             # return ((x,y), v)
             return board_index, value
         else:
             return None
+
+    
+    def get_block_possibilites(self, block_num: int, blocks: np.ndarray) -> np.ndarray:
+        return np.setdiff1d(self.options, blocks[block_num])
 
 
 
@@ -71,26 +86,45 @@ class Game():
         updated = True
         while updated:
             updated = False
-            for r in range(1, self.size + 1):
-                for c in range(1, self.size + 1):
-                    if board_copy[r-1][c-1] == 0:
+            for r in range(0, self.size):
+                for c in range(0, self.size):
+                    if board_copy[r][c] == 0:
                         possiblilites = self.get_row_column_intersection(r, c, board_copy)
                         if possiblilites.size == 1:
                             self.put_number(r, c, possiblilites[0], board_copy)
                             updated = True
-            self.create_blocks()
+            self.create_blocks(board_copy)
             for b in range(self.size):
                 output = self.get_block_intersection(b)
                 if output is not None:
                     coordinates, value = output
-                    r = coordinates[0] + 1
-                    c = coordinates[1] + 1 
-                    if board_copy[r-1][c-1] == 0:
+                    r = coordinates[0]
+                    c = coordinates[1] 
+                    if board_copy[r][c] == 0:
                         self.put_number(r, c, value, board_copy)
                         updated = True
-            self.create_blocks()
-            # print(board_copy)
+            self.create_blocks(board_copy)
         solved = False
         if 0 not in board_copy:
             solved = True
         return board_copy, solved
+
+    
+    def recursive_solve(self, board: np.ndarray, board_size: int) -> Tuple[np.ndarray, bool]:
+        if 0 not in board:
+            return board, True
+        for r in range(0, board_size):
+            for c in range(0, board_size):
+                if board[r][c] == 0:
+                    blocks = self.create_blocks(board)
+                    row_column_possibilities = self.get_row_column_intersection(r, c, board)
+                    current_block = self.get_block_from_coordinates(r, c)
+                    block_possibilities = self.get_block_possibilites(current_block, blocks)
+                    row_column_block_possiblilites = np.intersect1d(row_column_possibilities, block_possibilities)
+                    for p in row_column_block_possiblilites:
+                        board_copy = np.copy(board)
+                        self.put_number(r, c, p, board_copy)
+                        output_board, is_solved = self.recursive_solve(board_copy, board_size)
+                        if is_solved:
+                            return output_board, is_solved
+                    return board, False
--- tests/test_board.py
+++ tests/test_board.py
@@ -12,19 +12,29 @@ def test_board_dimensions():
 def test_set_number():
     game = Game(3)
     # user should not have to use zero index
-    game.put_number(row=1, column=1, number=3, board=game.board)
+    game.user_put_number(row=1, column=1, number=3, board=game.board)
     assert game.board[0][0] == 3
 
 
 def test_create_blocks():
     game = Game(4)
     game.board = np.array([[1,2,3,4],[3,4,1,2,],[2,3,4,1],[4,1,2,3]])
-    game.create_blocks()
+    game.create_blocks(game.board)
     
     expected_blocks = np.array([[1,2,3,4], [3,4,1,2], [2,3,4,1], [4,1,2,3]])
 
     assert np.array_equal(game.blocks, expected_blocks)
 
+
+def test_coordinate_to_block_mapping():
+    game = Game(4)
+    game.board = np.array([[1,2,3,4],[3,4,1,2,],[2,3,4,1],[4,1,2,3]])
+    game.create_blocks(game.board)
+
+    assert game.coordinate_to_block_mapping[(1,1)] == 0
+    assert game.coordinate_to_block_mapping[(0,2)] == 1
+    assert game.coordinate_to_block_mapping[(2,0)] == 2
+    assert game.coordinate_to_block_mapping[(2,2)] == 3
 
 def test_number_options():
     game = Game()
@@ -45,12 +55,12 @@ def test_possibilities():
             num = pre if pre % size == 0 else pre % size 
             board[i][j] = num
 
-    assert np.array_equal(game.get_row_column_intersection(1, 3, board), np.array([3]))
-    assert np.array_equal(game.get_row_column_intersection(2, 3, board), np.array([1]))
-    assert np.array_equal(game.get_row_column_intersection(3, 3, board), np.array([2]))
+    assert np.array_equal(game.get_row_column_intersection(0, 2, board), np.array([3]))
+    assert np.array_equal(game.get_row_column_intersection(1, 2, board), np.array([1]))
+    assert np.array_equal(game.get_row_column_intersection(2, 2, board), np.array([2]))
 
 
-def test_simple_solve():
+def test_simple_solve_mini():
     game = Game(3)
 
     board = game.board
@@ -69,4 +79,42 @@ def test_simple_solve():
 
 
     assert np.array_equal(board_output, solved)
+    assert is_solved
+
+
+def test_simple_solve_9x9():
+    game = Game()
+    game.board = np.array([[0,0,0,4,5,0,7,0,0],[0,0,0,0,8,0,2,3,6],[0,0,9,2,0,7,0,4,0],[0,0,5,0,0,0,9,0,4],[2,7,4,0,0,0,6,0,3],[3,9,0,5,0,0,8,0,0],[0,4,0,0,1,0,0,9,7],[7,0,1,0,4,0,5,0,8],[9,3,8,0,2,0,0,6,0]])
+    game.create_blocks(game.board)
+    output_board, is_solved = game.simple_solve()
+    print(output_board)
+    assert is_solved
+
+
+def test_recursive_solve_9x9():
+    game = Game()
+    game.board = np.array([[0,0,0,0,5,0,7,0,0],[0,0,0,0,8,0,2,3,6],[0,0,9,0,0,7,0,4,0],[0,0,5,0,0,0,9,0,4],[2,7,4,0,0,0,6,0,3],[3,9,0,5,0,0,8,0,0],[0,4,0,0,1,0,0,9,7],[7,0,1,0,4,0,5,0,8],[9,3,8,0,2,0,0,6,0]])
+    game.create_blocks(game.board)
+    output_board, is_solved = game.simple_solve()
+    output_board, is_solved = game.recursive_solve(output_board, 9)
+    print(output_board)
+    assert  is_solved
+
+def test_recursive_solve_blank_board_9x9():
+    game = Game()
+    game.board = np.array([[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]])
+    game.create_blocks(game.board)
+    output_board, is_solved = game.simple_solve()
+    output_board, is_solved = game.recursive_solve(output_board, 9)
+    print(output_board)
+    assert is_solved
+
+
+def test_supposed_hardest_sudoku_ever():
+    game = Game()
+    game.board = np.array([[8,0,0,0,0,0,0,0,0],[0,0,3,6,0,0,0,0,0],[0,7,0,0,9,0,2,0,0],[0,5,0,0,0,7,0,0,0],[0,0,0,0,4,5,7,0,0],[0,0,0,1,0,0,0,3,0],[0,0,1,0,0,0,0,6,8],[0,0,8,5,0,0,0,1,0],[0,9,0,0,0,0,4,0,0]])
+    game.create_blocks(game.board)
+    output_board, is_solved = game.simple_solve()
+    output_board, is_solved = game.recursive_solve(output_board, 9)
+    print(output_board)
     assert is_solved
